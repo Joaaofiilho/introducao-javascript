44f54447e36db15fc4ad906fbc748f044c06fde5
Joaaofiilho <joaot10112@gmail.com>
2017-07-01 15:07:15 -0300

    First commit

---
+++ js/principal.js
@@ -0,0 +1,50 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura invalida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura invalida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);   
+    }   
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("Cliquei no botão!"); 
+});
+
+
+console.log(botaoAdicionar);
